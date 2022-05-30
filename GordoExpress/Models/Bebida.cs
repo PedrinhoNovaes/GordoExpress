809003870da53d4d27af932e7994a11ccdaffa2d
Pedro Novaes <pe.fer@hotmail.com>
2022-05-29 23:50:57 -0300

    Data annotations aplicadas, migrations rodando e início dos repository.

--- GordoExpress/Models/Bebida.cs
+++ GordoExpress/Models/Bebida.cs
@@ -1,13 +1,27 @@
-﻿namespace GordoExpress.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace GordoExpress.Models
 {
+    [Table("Bebidas")]
     public class Bebida
     {
+        [Key]
         public int BebidaId { get; set; }
+        [StringLenght(80, MinimumLength = 4 ErrorMessage = "O Tamanho máximo é 80 caracteres")]
+        [Required(ErrorMessage = "Informe o nome da bebida.")]
+        [Display(Name = "Nome")]
         public string Nome { get; set; }
+        [Required(ErrorMessage = "Informe o preço da bebida.")]
+        [Display(Name = "Preço")]
+        [Column(TypeName = "decimal(10,2)")]
+        [Range(1,999.99,ErrorMessage ="O Preço deve estar entre 1 e 999,99")]
         public decimal Preco { get; set; }
         public string ImagemUrl { get; set; }
         public string ImagemThumbnailUrl { get; set; }
+        [Display(Name = "Preferido?")]
         public bool IsBebidaPreferido { get; set; }
+        [Display(Name = "Estoque")]
         public bool EmEstoque { get; set; }
 
         public int CategoriaId { get; set; }
--- GordoExpress/Models/Categoria.cs
+++ GordoExpress/Models/Categoria.cs
@@ -1,8 +1,16 @@
-﻿namespace GordoExpress.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace GordoExpress.Models
 {
+    [Table("Categorias")]
     public class Categoria
     {
+        [Key]
         public int CategoriaId { get; set; }
+        [StringLenght(100, ErrorMessage = "O Tamanho máximo é 100 caracteres")]
+        [Required(ErrorMessage ="Informe o nome da categoria.")]
+        [Display(Name = "Nome")]
         public string CategoriaNome { get; set; }
         public string Descricao { get; set; }
 
